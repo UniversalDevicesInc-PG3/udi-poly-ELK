a03f7e9da3eb09b81c1d19e5e2c83f01a7958fa5
Jim Searle <jimsearle@gmail.com>
2019-07-23 14:56:03 -0700

    add

---
+++ node_funcs.py
@@ -0,0 +1,21 @@
+
+import re
+
+# Removes invalid charaters for ISY Node description
+def get_valid_node_name(name):
+    # Only allow utf-8 characters
+    #  https://stackoverflow.com/questions/26541968/delete-every-non-utf-8-symbols-froms-string
+    name = bytes(name, 'utf-8').decode('utf-8','ignore')
+    # Remove <>`~!@#$%^&*(){}[]?/\;:"'` characters from name
+    return re.sub(r"[<>`~!@#$%^&*(){}[\]?/\\;:\"']+", "", name.lower()[:14])
+
+def get_profile_info(logger):
+    pvf = 'profile/version.txt'
+    try:
+        with open(pvf) as f:
+            pv = f.read().replace('\n', '')
+            f.close()
+    except Exception as err:
+        logger.error('get_profile_info: failed to read  file {0}: {1}'.format(pvf,err), exc_info=True)
+        pv = 0
+    return { 'version': pv }
