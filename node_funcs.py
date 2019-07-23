076435bc7ee69975c7315beb5d0e384620c37deb
JimBo <jimboca3@gmail.com>
2019-07-23 15:01:05 -0700

    Update node_funcs.py

--- node_funcs.py
+++ node_funcs.py
@@ -2,7 +2,7 @@
 import re
 
 # Removes invalid charaters for ISY Node description
-def get_valid_node_name(name):
+def get_valid_node_address(name):
     # Only allow utf-8 characters
     #  https://stackoverflow.com/questions/26541968/delete-every-non-utf-8-symbols-froms-string
     name = bytes(name, 'utf-8').decode('utf-8','ignore')
