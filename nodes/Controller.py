f8cde7b8fa74740cd0295b731a0c6ed4520e7b05
JimBo <jimboca3@gmail.com>
2023-12-30 08:46:21 -0800

    3.10.11 Fixed restarting PyISY

--- nodes/Controller.py
+++ nodes/Controller.py
@@ -867,7 +867,7 @@ class Controller(Node):
                 LOGGER.warning("Export Started")
                 self.lights_exported = {}
                 if not os.path.exists(persist_dir):
-                    os.makedir(persist_dir)
+                    os.makedirs(persist_dir)
                 fh = open(export_file, "w")
                 fh.write("<nodes>\n")
                 for (_, node) in self.pyisy.nodes:
